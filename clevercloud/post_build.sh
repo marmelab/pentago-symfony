dcb6fe3bd36c6838680947d638b476ed20a83b39
Clément Le Biez <clement@marmelab.com>
2021-02-02 17:00:30 +0100

    Do not execute database create

--- clevercloud/post_build.sh
+++ clevercloud/post_build.sh
@@ -1,7 +1,4 @@
 # This script will be run on clevercloud instance after each deploy.
 
-# Create DB if not exists
-php bin/console doctrine:database:create --if-not-exists
-
 # Play migrations without asking user input.
 php bin/console doctrine:migrations:migrate --no-interaction
