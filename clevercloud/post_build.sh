f28a1c4153fc3a1e8b2c19a62525f47d3ec4e4b3
Clément Le Biez <clement@marmelab.com>
2021-02-03 10:57:33 +0100

    post_build script cannot access to php but have ./bin/console on the root

--- clevercloud/post_build.sh
+++ clevercloud/post_build.sh
@@ -1,4 +1,4 @@
 # This script will be run on clevercloud instance after each deploy.
 
 # Play migrations without asking user input.
-php bin/console doctrine:migrations:migrate --no-interaction
+./bin/console doctrine:migrations:migrate --no-interaction
